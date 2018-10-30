24a76abe279b952d354a584812fa96899f4fe8fe
natoine <antoinseilles@gmail.com>
2018-10-30 17:03:26 +0100

    just changed route for scrape

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 var express = require('express')
 var app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+app.get('/scrape', function(req, res){
+
+  //All the web scraping magic will happen here
+
 })
 
 app.listen(3000, function () {
