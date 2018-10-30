8436ce4184447c15b48ebafbdcee6306b6db170a
natoine <antoinseilles@gmail.com>
2018-10-30 17:35:56 +0100

    now get google.com

--- server.js
+++ server.js
@@ -6,13 +6,13 @@ app.get('/scrape', function(req, res){
 
 	console.log("gonna scrape")
 
-	url = 'http://www.imdb.com/title/tt1229340/'
+	url = 'http://www.google.fr'
 
 	request(url, function(error, response, html){
 
-		console.log('error:', error); // Print the error if one occurred
-  		console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
   		console.log('body:', html);
+  		console.log('error:', error); // Print the error if one occurred
+  		console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
 
 		if(!error)
 		{
