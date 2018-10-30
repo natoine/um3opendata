43a001fd832bad0e33370911920016fe0ee31def
natoine <antoinseilles@gmail.com>
2018-10-30 17:22:22 +0100

    now can do http get on an imdb url and console log content

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
 var express = require('express')
 var app = express()
+var request = require('request')
 
 app.get('/scrape', function(req, res){
 
-  //All the web scraping magic will happen here
+	console.log("gonna scrape")
+
+	url = 'http://www.imdb.com/title/tt1229340/'
+
+	request(url, function(error, response, html){
+
+		console.log('error:', error); // Print the error if one occurred
+  		console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  		console.log('body:', html);
+
+		if(!error)
+		{
+
+		}
+	})
+
 
 })
 
